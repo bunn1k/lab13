36f9133a6af3489ff6ebe492fd6017b9fb1cccca
bunn1k <DISKO312@mail.ru>
2021-03-24 10:52:45 +0700

    Задание 15.1.2. reintroduce

--- Unit1.dfm
+++ Unit1.dfm
@@ -13,4 +13,30 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Button1: TButton
+    Left = 152
+    Top = 56
+    Width = 305
+    Height = 57
+    Caption = 'A:=TA.Create: A.show; A.Free;'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 152
+    Top = 119
+    Width = 305
+    Height = 57
+    Caption = 'B:=TB.Create: B.show; B.Free;'
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 152
+    Top = 182
+    Width = 305
+    Height = 57
+    Caption = 'D:=TD.Create: D.show; D.Free;'
+    TabOrder = 2
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,49 +4,136 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
   end;
 
-  TFirst = class
+  TA = class
   public
-  procedure Hello;
+    procedure show; virtual;
   end;
 
-  TSecond = class(TFirst)
+  TB = class(TA)
   public
-  procedure Hello; reintroduce;
+    procedure show; override;
+  end;
+
+  TC = class(TB)
+  public
+    procedure show; override;
+  end;
+
+  TD = class(TC)
+  public
+    procedure show; reintroduce;
+  end;
+
+  TE = class(TD)
+  public
+    procedure show; virtual;
+  end;
+
+  TF = class(TE)
+  public
+    procedure show; override;
+  end;
+
+  TG = class(TF)
+  public
+    procedure show; override;
   end;
 
 var
   Form1: TForm1;
+  A:TA;
+  B:TB;
+  C:TC;
+  D:TD;
+  E:TE;
+  F:TF;
+  G:TG;
 
 implementation
 
 {$R *.dfm}
 
-{ TFirst }
+{ TA }
 
-procedure TFirst.Hello;
-var
-  a:string;
+procedure TA.show;
 begin
-  a:='Hello';
+  ShowMessage('TA.show');
 end;
 
-{ TSecond }
+{ TB }
 
-procedure TSecond.Hello;
-var
-  a:string;
+procedure TB.show;
 begin
-  a:='Hello Second';
+  ShowMessage('TB.show');
+end;
+
+{ TC }
+
+procedure TC.show;
+begin
+  ShowMessage('TC.show');
+end;
+
+{ TD }
+
+procedure TD.show;
+begin
+  ShowMessage('TD.show');
+end;
+
+{ TE }
+
+procedure TE.show;
+begin
+  ShowMessage('TE.show');
+end;
+
+{ TF }
+
+procedure TF.show;
+begin
+  ShowMessage('TF.show');
+end;
+
+{ TG }
+
+procedure TG.show;
+begin
+  ShowMessage('TG.show');
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  A:=TA.Create; A.show; A.Free;
+  A:=TB.Create; A.show; A.Free;
+  A:=TC.Create; A.show; A.Free;
+  A:=TD.Create; A.show; A.Free;
+  A:=TE.Create; A.show; A.Free;
+  A:=TF.Create; A.show; A.Free;
+  A:=TG.Create; A.show; A.Free;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  B:=TB.Create; B.show; B.Free;
+  B:=TC.Create; B.show; B.Free;
+  B:=TD.Create; B.show; B.Free;
+  B:=TE.Create; B.show; B.Free;
 end;
 
 end.
