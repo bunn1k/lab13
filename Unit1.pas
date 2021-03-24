9b64d6145a205adbb1fdbb5cf62f2770e40a6878
bunn1k <DISKO312@mail.ru>
2021-03-24 10:20:58 +0700

    Задание 15.1.1

--- Unit1.pas
+++ Unit1.pas
@@ -14,11 +14,39 @@ type
     { Public declarations }
   end;
 
+  TFirst = class
+  public
+  procedure Hello;
+  end;
+
+  TSecond = class(TFirst)
+  public
+  procedure Hello; reintroduce;
+  end;
+
 var
   Form1: TForm1;
 
 implementation
 
 {$R *.dfm}
+
+{ TFirst }
+
+procedure TFirst.Hello;
+var
+  a:string;
+begin
+  a:='Hello';
+end;
+
+{ TSecond }
+
+procedure TSecond.Hello;
+var
+  a:string;
+begin
+  a:='Hello Second';
+end;
 
 end.
